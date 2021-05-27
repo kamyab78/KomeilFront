012f2c15375db875b8d04a69dd657912f23cf16b
Your Name <you@example.com>
2021-05-27 23:37:40 +0430

    Add headers

--- src/komeil/Component/Header/Headers.tsx
+++ src/komeil/Component/Header/Headers.tsx
@@ -18,61 +18,67 @@ const Headers = () => {
         <div className="row main-div-header">
             <div className='col'>
                 <div className='row'>
-                    <div className="col-7 search-div ">
+            
+
+<div className='col-2 profile-card-div'>
+<div className='shoping-div'>
+       <i className="material-icons-outlined">
+shopping_bag
+</i>
+       </div>
+   <div className='profile-div'>
+   <i className="material-icons-outlined">
+person
+</i>
+       </div>
+    
+</div>
+                    <Link to='/' className=" col-5 logo-div" >
+                        <img alt="" />
+                       
+                    </Link>
+                    <div className="col-4 search-div ">
                         <div className='box'>
-                            <input></input>
+                            <input placeholder='جستجوی محصولات در کمیل شاپ'></input>
                             <SearchIcon className='seachIcon' />
                         </div>
 
 
                     </div>
+                    <div className="col-1">
+                    
 
-
-                    <Link to='/' className=" col-5 logo-div" >
-                        <img src={Logo} alt="" />
-                        <h1>تاپ نور</h1>
-                    </Link>
+                    </div>
                 </div>
                 <div className='row bottom-box'>
 
-                    <div className='col-3 prof-box'>
-                        <Dropdown className="dropdownClasss">
-                            <Dropdown.Toggle variant="none" className="dropdownToggleClasss">
-                                <i className="material-icons">account_circle</i>
-                                <h6 className="d-none d-md-block">علی</h6>
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item>
-                                    <Link
-to='/profile/personal'
-                                        className="dropdowm-item"
-                                    >
-                                        <i className="material-icons">account_circle</i>
-                                        <h6>پروفایل</h6>
-                                    </Link>
-                                </Dropdown.Item>
-
-                                <Dropdown.Item>
-                                    <div className="dropdowm-item">
-                                        <i className="material-icons">logout</i>
-                                        <h6>خروج</h6>
-                                    </div>
-                                </Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        <Link className="material-icons-outlined bascket" to='/cart'>
-                            shopping_cart
-</Link>
+                    <div className='col-4 prof-box'>
+<h6>کمترین قیمت، بهترین کیفیت</h6>
                     </div>
-                    <Link to='/contactus' className="col-1 item-box" >تماس با ما</Link>
-                    <Link to='/aboutus' className="col-1 item-box" >درباره ما</Link>
-                    <Link to='/blog' className="col-1 item-box" >بلاگ</Link>
-                    <Link to='/moshavere' className="col-1 item-box" >مشاوره</Link>
-                    <Link to='/bazarche' className="col-1 item-box" >بازارچه</Link>
-                    <Link to='/home' className="col-1 item-box" >خانه</Link>
-                    <Link to='/bazarche' className="col-2 category-box" >دسته بندی</Link>
-                    <div className='col-1'></div>
+               <div className='col-2'></div>
+                    <Link to='/aboutus' className="col-1 item-box" ><h5>درباره ما</h5></Link>
+                   <Link to='/contactus' className="col-1 item-box" ><h5>تماس با ما</h5></Link>
+                    <Link to='/bazarche' className="col-2 item-box" >
+                        <div className='promotion-box'>
+                                               <h6>
+                          کالاهای تخفیف دار
+                          
+                          </h6>  
+                          <i className="material-icons-outlined">
+local_offer
+</i> 
+                        </div>
+  
+                        </Link>
+                   
+                    <Link to='/' className="col-2 category-box" >
+
+                        دسته بندی
+                        <i className="material-icons-outlined">
+reorder
+</i>
+                    </Link>
+                    
 
 
                 </div>
